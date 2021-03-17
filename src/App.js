865cdaf0f7fd82bbe70c1184d282a3edf2e5e108
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-17 13:46:26 -0300

    feat: Passing method references between components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg'
+import './App.css'
+import Person from './Person/Person'
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Goku', age: 30 },
+      { name: 'Bulma', age: 22 },
+      { name: 'Kuririm', age: 34 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!')
+    console.log(this.state)
+    // Don'T DO THIS: this.state.persons[0].name = 'Gohan'
+    this.setState({
+      persons: [
+        { name: newName, age: 30 },
+        { name: 'Bulma', age: 22 },
+        { name: 'Titi', age: 18 }
+      ]
+    })
+  }
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className='App-link'
+            href='https://reactjs.org'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Learn React
+          </a>
+          <button onClick={() => this.switchNameHandler('Titi')}>Switch name</button>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.switchNameHandler.bind(this, 'Videl')}
+          >
+            My Hobbies: Racing
+          </Person>
+          <Person
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+          />
+        </header>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+const person = props => {
+  return (
+    <div>
+      <p onClick={props.click}>
+        I'm {props.name} and I am {props.age} years old!
+      </p>
+      <p>{props.children}</p>
+    </div>
+  )
+}
+
+export default person
