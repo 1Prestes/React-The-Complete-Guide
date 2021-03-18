ea477e6385f952dae7f52bec7544390e6a712255
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-18 14:58:27 -0300

    feat: using Radium for media queries

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Radium, { StyleRoot } from 'radium'
 
 import './App.css'
 import Person from './Person/Person'
@@ -59,7 +60,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
     let persons = null
 
@@ -80,6 +85,10 @@ class App extends Component {
         </div>
       )
       style.backgroundColor = 'red'
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      }
     }
 
     const classes = []
@@ -92,17 +101,19 @@ class App extends Component {
     }
 
     return (
-      <div className='App'>
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!</p>
+      <StyleRoot>
+        <div className='App'>
+          <h1>Hi, I'm a React App</h1>
+          <p className={classes.join(' ')}>This is really working!</p>
 
-        <button style={style} onClick={this.togglePersonsHandler}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
+          <button style={style} onClick={this.togglePersonsHandler}>
+            Toggle Persons
+          </button>
+          {persons}
+        </div>
+      </StyleRoot>
     )
   }
 }
 
-export default App
+export default Radium(App)
