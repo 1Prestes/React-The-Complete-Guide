dcb267a30e7dfdc2b3a0e5cb9b5dba36b46e8bc5
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-19 11:23:33 -0300

    feat: component creation lifecycle in action

--- react-complete-guide/src/containers/App.js
+++ react-complete-guide/src/containers/App.js
@@ -5,6 +5,11 @@ import Persons from '../components/Persons/Persons'
 import Cockpit from '../components/Cockpit/Cockpit'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    console.log('[App.js] constructor')
+  }
+
   state = {
     persons: [
       { id: 'adg1', name: 'Goku', age: 30 },
@@ -13,6 +18,19 @@ class App extends Component {
     ],
     otherState: 'some other value',
     showPersons: false
+  }
+
+  static getDerivedStateFromProps (props, state) {
+    console.log('[App.js] getDerivedStateFromProps', props)
+    return state
+  }
+
+  // componentWillMount () {
+  //   console.log('[App.js] componentWillMount')
+  // }
+
+  componentDidMount () {
+    console.log('[App.js] componentDidMount')
   }
 
   switchNameHandler = newName => {
@@ -51,6 +69,7 @@ class App extends Component {
   }
 
   render () {
+    console.log('[App.js] render')
     let persons = null
 
     if (this.state.showPersons) {
@@ -66,6 +85,7 @@ class App extends Component {
     return (
       <div className={classes.App}>
         <Cockpit
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
