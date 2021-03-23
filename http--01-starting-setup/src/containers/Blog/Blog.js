8ec4728bda1ad6cc31b0a00436ff06b9865d14d5
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-23 17:57:52 -0300

    feat: handle errors locally

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -7,46 +7,54 @@ import NewPost from '../../components/NewPost/NewPost'
 import './Blog.css'
 
 const Blog = () => {
-  const [posts, setPosts] = useState([])
+  const [loadedPosts, setLoadedPosts] = useState([])
   const [selectedPostId, setSelectedPostId] = useState(null)
+  const [error, setError] = useState(false)
 
   useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-      const posts = response.data.slice(0, 4)
-      const updatedPosts = posts.map(post => ({
-        ...post,
-        author: 'Max'
-      }))
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        const posts = response.data.slice(0, 4)
+        const updatedPosts = posts.map(post => ({
+          ...post,
+          author: 'Max'
+        }))
 
-      return setPosts(updatedPosts)
-    })
+        return setLoadedPosts(updatedPosts)
+      })
+      .catch(error => setError(true))
   }, [])
 
   const postSelectedHandler = id => {
     setSelectedPostId(id)
   }
+  let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>
 
-  return (
-    <div>
-      <section className='Posts'>
-        {posts &&
-          posts.map(post => (
-            <Post
-              key={post.id}
-              title={post.title}
-              author={post.author}
-              clicked={() => postSelectedHandler(post.id)}
-            />
-          ))}
-      </section>
-      <section>
-        <FullPost id={selectedPostId} />
-      </section>
-      <section>
-        <NewPost />
-      </section>
-    </div>
-  )
+  if (!error) {
+    posts = (
+      <div>
+        <section className='Posts'>
+          {loadedPosts &&
+            loadedPosts.map(post => (
+              <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => postSelectedHandler(post.id)}
+              />
+            ))}
+        </section>
+        <section>
+          <FullPost id={selectedPostId} />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    )
+  }
+  return posts
 }
 
 export default Blog
