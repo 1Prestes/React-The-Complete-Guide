b9257e3cb756df4ba5cb34610e7d9a9c27919f5c
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-23 15:05:45 -0300

    feat: transform data

--- http--01-starting-setup/src/containers/Blog/Blog.js
+++ http--01-starting-setup/src/containers/Blog/Blog.js
@@ -5,20 +5,30 @@ import Post from '../../components/Post/Post'
 import FullPost from '../../components/FullPost/FullPost'
 import NewPost from '../../components/NewPost/NewPost'
 import './Blog.css'
+import post from '../../components/Post/Post'
 
 const Blog = () => {
   const [posts, setPosts] = useState([])
 
   useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/posts')
-      .then(response => setPosts(response.data))
-  })
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+      const posts = response.data.slice(0, 4)
+      const updatedPosts = posts.map(post => ({
+        ...post,
+        author: 'Max'
+      }))
+
+      return setPosts(updatedPosts)
+    })
+  }, [])
 
   return (
     <div>
       <section className='Posts'>
-        {posts && posts.map(post => <Post key={post.id} title={post.title} />)}
+        {posts &&
+          posts.map(post => (
+            <Post key={post.id} title={post.title} author={post.author} />
+          ))}
       </section>
       <section>
         <FullPost />
