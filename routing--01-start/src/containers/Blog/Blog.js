dc730ec0629b81b09ed34560aceb01a95b2b351e
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-25 15:24:36 -0300

    style: style the active route

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 // import axios from 'axios'
 
 import Posts from './Posts/Posts'
@@ -15,10 +15,20 @@ const Blog = () => {
         <nav>
           <ul>
             <li>
-              <Link to='/'>Home</Link>
+              <NavLink
+                to='/'
+                exact
+                activeClassName='my-active'
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}
+              >
+                Home
+              </NavLink>
             </li>
             <li>
-              <Link
+              <NavLink
                 to={{
                   pathname: '/new-post',
                   hash: '#submit',
@@ -26,7 +36,7 @@ const Blog = () => {
                 }}
               >
                 New Post
-              </Link>
+              </NavLink>
             </li>
           </ul>
         </nav>
