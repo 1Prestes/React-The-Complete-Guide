c5f4f9b2937a85397726e127f86de07df6c379a1
Dijango Alves <dijango_ilha@hotmail.com>
2021-03-25 16:15:22 -0300

    feat: use switch to load a single route

--- routing--01-start/src/containers/Blog/Blog.js
+++ routing--01-start/src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Route, NavLink } from 'react-router-dom'
+import { Route, NavLink, Switch } from 'react-router-dom'
 // import axios from 'axios'
 
 import Posts from './Posts/Posts'
@@ -43,9 +43,11 @@ const Blog = () => {
           </ul>
         </nav>
       </header>
-      <Route path='/' exact component={Posts} />
-      <Route path='/new-post' component={NewPost} />
-      <Route path='/:id' exact component={FullPost} />
+      <Switch>
+        <Route path='/' exact component={Posts} />
+        <Route path='/new-post' component={NewPost} />
+        <Route path='/:id' exact component={FullPost} />
+      </Switch>
     </div>
   )
 }
