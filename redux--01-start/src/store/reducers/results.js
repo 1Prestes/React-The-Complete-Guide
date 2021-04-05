a9ca8bb5dce90ee7a25b53f7c7bce254cc57f108
Dijango Alves <dijango_ilha@hotmail.com>
2021-04-05 10:46:04 -0300

    feat: use utility functions

--- redux--01-start/src/store/reducers/results.js
+++ redux--01-start/src/store/reducers/results.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import { updateObject } from '../utility'
 
 const initialState = {
   results: []
@@ -7,18 +8,14 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         results: state.results.concat({ id: new Date(), value: action.result })
-      }
+      })
     case actionTypes.DELETE_RESULT:
       const updatedArray = state.results.filter(
         item => item.id !== action.resultElementId
       )
-      return {
-        ...state,
-        results: updatedArray
-      }
+      return updateObject(state, { results: updatedArray })
     default:
       return state
   }
